760cf6341fd193a61bd48840bffe40f58156fe99
Alexandr Kozlenkov <sah4ez32@gmail.com>
2017-11-05 22:07:38 +0300

    init commit

---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	"github.com/mattes/migrate"
+	"github.com/mattes/migrate/database/postgres"
+	_ "github.com/mattes/migrate/source/file"
+)
+
+var (
+	db      *sql.DB
+	connStr string
+)
+
+func init() {
+	pgUser := os.Getenv("POSTGRES_USER")
+	pgPassword := os.Getenv("POSTGRES_PASSWORD")
+	pgDb := os.Getenv("POSTGRES_DB")
+
+	connStr = "user=" + pgUser + " dbname=" + pgDb + " password=" + pgPassword + " sslmode=disable"
+
+	db, err := sql.Open("postgres", connStr)
+	checkError(err)
+
+	driver, err := postgres.WithInstance(db, &postgres.Config{})
+	checkError(err)
+
+	m, err := migrate.NewWithDatabaseInstance(
+		"file://./migrations",
+		pgDb,
+		driver)
+	checkError(err)
+
+	err = m.Up()
+	checkError(err)
+}
+
+func main() {
+	db, err := sql.Open("postgres", connStr)
+	checkError(err)
+	defer db.Close()
+	err = db.Ping()
+	checkError(err)
+	fmt.Println("final")
+
+}
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
